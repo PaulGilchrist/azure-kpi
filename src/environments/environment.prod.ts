5d043d61449d07690dfedc48f91763acac53020c
Paul Gilchrist <paul.gilchrist@outlook.com>
2020-05-21 14:02:15 -0400

    Added azureAuthProvider

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,12 @@ export const environment = {
     appInsights: {
         instrumentationKey: '6cf7b8be-bdd5-4505-a9f8-fb68f1f594c0'
     },
+    azureAuthProvider: {
+        aadInstance: 'https://login.microsoftonline.com/{0}',
+        clientId: 'bd065891-b008-4968-9b26-5f2bcb9c1b66',
+        domainHint: 'pulte.com',
+        tenant: 'pulte.onmicrosoft.com'
+    },
     envName: 'production',
     production: true
 };
